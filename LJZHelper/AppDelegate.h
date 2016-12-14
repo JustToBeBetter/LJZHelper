bd3a812be39751ff34232e94bce871f042a52f08
JustToBeBetter <likeme@li.local>
2016-12-14 16:06:56 +0800

    Initial Commit

---
+++ LJZHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJZHelper
+//
+//  Created by DBOX on 2016/12/14.
+//  Copyright © 2016年 DBOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJZHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJZHelper
+//
+//  Created by DBOX on 2016/12/14.
+//  Copyright © 2016年 DBOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
